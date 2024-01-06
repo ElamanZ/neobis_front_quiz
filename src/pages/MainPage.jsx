2b1d65462a27ec3c83ffdcc717dc84a34820c982
elamanz <z.elaman699@gmail.com>
2024-01-06 20:49:32 +0600

    add sweper.js

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'swiper/css';
 import styles from './styles.module.scss'
 import arrow_icon from '../assets/images/arrow_icon.svg'
 import card_icon1 from '../assets/images/card_icon11.png'
@@ -9,7 +10,71 @@ import card_quiz_icon1 from '../assets/images/card_quiz_icon11.png'
 import card_quiz_icon2 from '../assets/images/card_quiz_icon22.png'
 import card_quiz_icon3 from '../assets/images/card_quiz_icon33.png'
 import card_quiz_icon4 from '../assets/images/card_quiz_icon44.png'
+import {Swiper, SwiperSlide} from "swiper/react";
 function MainPage(props) {
+
+    const articleCardsData = [
+        {
+            id: 1,
+            title: 'Жизнь и правление Наполеона Бонапарта',
+            image: card_icon1,
+            category: 'История',
+            backgroundColor: '#FFE0A3'
+        },
+        {
+            id: 2,
+            title: 'Философия Аристотеля',
+            image: card_icon2,
+            category: 'Философия',
+            backgroundColor: '#CCFFF6'
+        },
+        {
+            id: 3,
+            title: 'Почему Чехов не так прост?',
+            image: card_icon3,
+            category: 'Литература',
+            backgroundColor: '#FFCCFD'
+        },
+        {
+            id: 4,
+            title: 'Почему вы неправильно поняли «Мастера и Маргариту»?',
+            image: card_icon4,
+            category: 'Литература',
+            backgroundColor: '#ADD3FF'
+        }
+    ];
+
+    const quizCardsData = [
+        {
+            id: 1,
+            title: 'История',
+            image: card_quiz_icon1,
+            text: '10 вопросов',
+            backgroundColor: '#FFA9A3'
+        },
+        {
+            id: 2,
+            title: 'История',
+            image: card_quiz_icon2,
+            text: '10 вопросов',
+            backgroundColor: '#ADD3FF'
+        },
+        {
+            id: 3,
+            title: 'Философия',
+            image: card_quiz_icon3,
+            text: '10 вопросов',
+            backgroundColor: '#CBF89E'
+        },
+        {
+            id: 4,
+            title: 'Психология',
+            image: card_quiz_icon4,
+            text: '20 вопросов',
+            backgroundColor: '#DAB5FF'
+        }
+    ];
+
     return (
         <div className='container'>
             <div className={styles.title}>
@@ -17,29 +82,23 @@ function MainPage(props) {
                 <img src={arrow_icon} alt="arrow_icon"/>
             </div>
             <div className={styles.articleCards}>
-                <div className={styles.articleCard} style={{ backgroundColor: '#FFE0A3' }}>
-                    <p>Жизнь и правление Наполеона Бонапарта</p>
-                    <img src={card_icon1} alt="card_icon1"/>
-                    <span>#История</span>
-                </div>
+                <Swiper
 
-                <div className={styles.articleCard} style={{ backgroundColor: '#CCFFF6' }}>
-                    <p>Философия Аристотеля</p>
-                    <img src={card_icon2} alt="card_icon2"/>
-                    <span>#Философия</span>
-                </div>
-
-                <div className={styles.articleCard} style={{ backgroundColor: '#FFCCFD' }}>
-                    <p>Почему Чехов не так прост?</p>
-                    <img src={card_icon3} alt="card_icon3"/>
-                    <span>#Литература</span>
-                </div>
-
-                <div className={styles.articleCard} style={{ backgroundColor: '#ADD3FF' }}>
-                    <p>Почему вы неправильно поняли «Мастера и Маргариту»?</p>
-                    <img src={card_icon4} alt="card_icon4"/>
-                    <span>#Литература</span>
-                </div>
+                    spaceBetween={-50}
+                    slidesPerView={3}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                >
+                    {articleCardsData.map((articleData) => (
+                        <SwiperSlide key={articleData.id}>
+                            <div className={styles.articleCard} style={{ backgroundColor: articleData.backgroundColor }}>
+                                <p>{articleData.title}</p>
+                                <img src={articleData.image} alt={`card_icon${articleData.id}`} />
+                                <span>#{articleData.category}</span>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
 
             </div>
 
@@ -49,29 +108,15 @@ function MainPage(props) {
             </div>
 
             <div className={styles.quiz_cards}>
-                <div className={styles.quiz_card} style={{ backgroundColor: '#FFA9A3' }}>
-                    <img src={card_quiz_icon1} alt="card_quiz_icon1"/>
-                    <p>История</p>
-                    <span>10 вопросов</span>
-                </div>
 
-                <div className={styles.quiz_card} style={{ backgroundColor: '#ADD3FF' }}>
-                    <img src={card_quiz_icon2} alt="card_quiz_icon2"/>
-                    <p>Литература</p>
-                    <span>15 вопросов</span>
-                </div>
+                {quizCardsData.map((quizData) => (
+                    <div className={styles.quiz_card} style={{ backgroundColor: quizData.backgroundColor }}>
+                        <img src={quizData.image} alt="card_quiz_icon1"/>
+                        <p>{quizData.title}</p>
+                        <span>{quizData.text}</span>
+                    </div>
+                ))}
 
-                <div className={styles.quiz_card} style={{ backgroundColor: '#CBF89E' }}>
-                    <img src={card_quiz_icon3} alt="card_quiz_icon3"/>
-                    <p>Философия</p>
-                    <span>10 вопросов</span>
-                </div>
-
-                <div className={styles.quiz_card} style={{ backgroundColor: '#DAB5FF' }}>
-                    <img src={card_quiz_icon4} alt="card_quiz_icon4"/>
-                    <p>Психология</p>
-                    <span>20 вопросов</span>
-                </div>
             </div>
         </div>
     );
