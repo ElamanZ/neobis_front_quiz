01ca8f3f5553dedb72705f60ad0d56ad7b327693
elamanz <z.elaman699@gmail.com>
2024-01-16 14:25:18 +0600

    fix index/js

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 // В вашем файле, где настраивается store, должны быть добавлены middleware
 import { configureStore } from '@reduxjs/toolkit';
-import articlesReducer from '../store/slices/articlesSlice.js';
-import quizesReducer from '../store/slices/quizesSlice.js';
-import quizTestReducer from './slices/quizTestSlice.js';
+import articlesReducer from '../store/slices/articlesSlice';
+import quizesReducer from '../store/slices/quizesSlice';
+import quizTestReducer from './slices/quizTestSlice';
 
 const store = configureStore({
     reducer: {
